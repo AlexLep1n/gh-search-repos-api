d5a644f2fed3bcf0cddd95baf9481ea37ec244d4
Alex Lepin <alexlepin@mail.ru>
2024-08-22 22:10:10 +0300

    change: change func

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
 const searchInput = document.querySelector(".wrapper__search-input");
 const resultList = document.querySelector(".wrapper__result-list");
 const resultItems = document.querySelectorAll(".wrapper__result-item");
+const cards = document.querySelector(".cards");
 
-const gitHubReposUrl = "https://api.github.com/repositories";
+const gitHubReposUrl =
+  "https://api.github.com/search/repositories?q=${queryText}&per_page=5";
 
-async function getGitHubRepos() {
+// Получение массива репозиториев с GitHub API
+async function getGitHubRepos(queryText) {
   try {
-    const response = await fetch(gitHubReposUrl);
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=${queryText}&per_page=5`
+    );
     if (!response.ok) {
       throw new Error(`Request failed, status ${response.status}`);
     }
@@ -19,22 +24,23 @@ async function getGitHubRepos() {
   }
 }
 
+// Сортировка массива репозиториев по данным input`a и вывод их имен
+
+let filteredRepos;
+
 async function showResults(repoName) {
-  const reposData = await getGitHubRepos();
-  console.log(repoName);
   if (repoName.trim()) {
-    let firstFive = reposData
-      .filter((repo) => {
-        if (repo.name) {
-          return repo.name
-            .toLocaleLowerCase()
-            .startsWith(repoName.toLocaleLowerCase());
-        }
-      })
-      .slice(0, 5);
+    const reposData = await getGitHubRepos(repoName);
+    filteredRepos = reposData.items.filter((repo) => {
+      if (repo.name) {
+        return repo.name
+          .toLocaleLowerCase()
+          .startsWith(repoName.toLocaleLowerCase());
+      }
+    });
     resultItems.forEach((li, i) => {
-      if (firstFive[i]?.name) {
-        li.textContent = firstFive[i].name;
+      if (filteredRepos[i]?.name) {
+        li.textContent = filteredRepos[i].name;
         li.style.display = "block";
       } else {
         li.style.display = "none";
@@ -46,9 +52,10 @@ async function showResults(repoName) {
   }
 }
 
+// Обработка события input
 if (searchInput) {
   searchInput.addEventListener("input", (e) => {
-    debounce(showResults, 400)(e.target.value);
+    debounce(showResults, 700)(e.target.value);
   });
 }
 
@@ -56,8 +63,42 @@ function debounce(fn, delay) {
   let timerID;
   return function (...args) {
     clearTimeout(timerID);
-    timerID = setTimeout(() => {
-      fn.apply(this, args);
-    }, delay);
+    timerID = setTimeout(() => fn.apply(this, args), delay);
   };
+}
+
+function createCard(cardData) {
+  cards.insertAdjacentHTML(
+    "beforeend",
+    `<div class="cards__item">
+      <div class="cards__item-content">
+      <p class="cards__item-text">Name: ${cardData.name}</p>
+      <p class="cards__item-text">Owner: ${cardData.owner.login}</p>
+      <p class="cards__item-text">Stars: </p>
+      </div>
+      <button class="cards__item-close"></button>
+    </div>`
+  );
+}
+
+const cardsList = [];
+
+resultList.addEventListener("click", (e) => showCards(e));
+
+function showCards(e) {
+  let currentLi = e.target;
+  if (currentLi.tagName === "LI") {
+    let newCardData = filteredRepos.find(
+      (item) => item.name === currentLi.textContent
+    );
+    if (!cardsList.includes(newCardData)) {
+      cardsList.push(newCardData);
+      createCard(newCardData);
+    }
+    if (cards.children.length > 0) {
+      cards.style.display = "flex";
+    } else {
+      cards.style.display = "none";
+    }
+  }
 }
